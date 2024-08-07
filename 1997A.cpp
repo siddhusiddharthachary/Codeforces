3aed474b4bd463d41d88239bd192de59ad608daf
siddhusiddharthachary <151196424+siddhusiddharthachary@users.noreply.github.com>
2024-08-07 14:39:10 +0530

    Update 1997A.cpp

--- 1997A.cpp
+++ 1997A.cpp
@@ -1,62 +1,62 @@
-#include<stdio.h>
-#include<vector>
-#include<iostream>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int timeTaken(string s){
+
+int timeTaken(string s) {
     int time = 2;
-    for(int i=1;i<s.size();i++){
-        if(s[i]==s[i-1]){
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] == s[i - 1]) {
             time++;
-        }else{
+        } else {
             time += 2;
         }
     }
     return time;
 }
-string password(string s){
+
+string password(string s) {
     int maxTime = 0;
     string ans;
-    for(char ch = 'a'; ch<='z'; ++ch){
+
+    // Inserting at the beginning
+    for (char ch = 'a'; ch <= 'z'; ++ch) {
         string newS = ch + s;
-        if(timeTaken(newS)>maxTime){
+        int currentTime = timeTaken(newS);
+        if (currentTime > maxTime) {
             ans = newS;
-            maxTime = timeTaken(newS);
+            maxTime = currentTime;
         }
     }
-    for(int i=0; i<s.size(); i++){
-        for(char ch='a'; ch<='z'; ++ch){
-            string newS  = s.substr(0, i) + ch + s.substr(i);
-            if(timeTaken(newS)>maxTime){
+
+    // Inserting in the middle
+    for (int i = 0; i <= s.size(); i++) {
+        for (char ch = 'a'; ch <= 'z'; ++ch) {
+            string newS = s.substr(0, i) + ch + s.substr(i);
+            int currentTime = timeTaken(newS);
+            if (currentTime > maxTime) {
                 ans = newS;
-                maxTime = timeTaken(newS);   
+                maxTime = currentTime;
             }
         }
     }
-    for(char ch = 'a'; ch<='z'; ++ch){
-        string newS = s + ch;
-        if(timeTaken(newS)>maxTime){
-            ans = newS;
-            maxTime = timeTaken(newS);
-        }
-    }
+
     return ans;
 }
-int main(){
-    // vector<vector<int>> question;
+
+int main() {
     int testCases;
-    // cout<<"enter number of testcases"<<endl;
-    cin>>testCases;
-    vector<string> arr;
-    for(int i=0;i<testCases;i++){
-        string s;
-        // cout<<"enter the string"<<endl;
-        cin>>s;
-        arr.push_back(s);
+    cin >> testCases;
+    vector<string> arr(testCases);
+    
+    for (int i = 0; i < testCases; i++) {
+        cin >> arr[i];
     }
-    for(int i=0;i<testCases;i++){
-        // string s = "password";
-        cout<<arr[i]<<endl;
+    
+    for (int i = 0; i < testCases; i++) {
+        cout << password(arr[i]) << endl;
     }
+    
     return 0;
 }
