5a0b4f1c7bb57ec3585603e29d61110bde6ec07d
siddhusiddharthachary <151196424+siddhusiddharthachary@users.noreply.github.com>
2024-12-30 20:59:53 +0530

    Add files via upload

---
+++ 1945B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t; 
+
+    while (t--) {
+        long long a, b, m;
+        cin >> a >> b >> m;
+        cout << ((m/a)+1)+((m/b)+1) << endl;
+    }
+
+    return 0;
+}
